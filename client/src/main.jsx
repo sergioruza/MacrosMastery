fca87292a2b2c549c6cd9daecdfdd01d61c6ff03
Sergio <sergioruza22@gmail.com>
2023-06-01 17:23:32 -0300

    feat: login form

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './routes/Home.jsx'
-import Login from './routes/Login.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
 
 const router = createBrowserRouter([
   {
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,53 @@
+import { useState } from "react"
+
 function Login() {
+  const [email, setEmail] = useState('')
+  const [emailConfirm, setEmailConfirm] = useState('')
+  const [passwordConfirm, SetPasswordConfirm] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <div>Login</div>
+    <div>
+      <form>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}>
+          </input>
+        </label>
+
+        <label>
+          Confirme seu email:
+          <input
+            type="email"
+            value={emailConfirm}
+            onChange={({ target }) => setEmailConfirm(target.value)}>
+          </input>
+        </label>
+
+        <label>
+          Senha:
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}>
+          </input>
+        </label>
+
+        <label>
+          Confirme sua senha:
+          <input
+            type="password"
+            value={passwordConfirm}
+            onChange={({ target }) => SetPasswordConfirm(target.value)}>
+          </input>
+        </label>
+
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
   )
 }
 
