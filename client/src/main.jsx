9e5a60c456cac4b143a816146a61f27861f0548f
Sergio <sergioruza22@gmail.com>
2023-06-03 11:59:59 -0300

    feat: login route along with the form

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
     path: "/register",
     element: <Register />
   },
+  {
+    path: "/login",
+    element: <Login />
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
